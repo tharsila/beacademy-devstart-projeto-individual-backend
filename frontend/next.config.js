8526e9b284b8789f154060336d289e7cb9a9bfc4
tharsila <tharsilapaula@gmail.com>
2022-07-11 08:49:50 -0300

    chore: adding new config for images domains

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,6 @@ const nextConfig = {
 
 module.exports = {
   nextConfig,
-  images: { domains: ['*', 'petz.com.br', 'www.petz.com.br'] }
+  images: { domains: ['*', 'petz.com.br', 'www.petz.com.br', 'images.pexels.com', 'wwww.images.pexels.com'] }
 
 }
