77a04f36783e304a169e5e97274803b58d7cc97e
tharsila <tharsilapaula@gmail.com>
2022-07-17 22:25:28 -0300

    feat: add page 404

--- frontend/pages/404.js
+++ frontend/pages/404.js
@@ -1,14 +1,17 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
 export default function NotFound() {
   return (
     <>
       <Head>
         <title>Página não encontrada</title>
       </Head>
-      <h1>404</h1>
-      <p>Parece que essa página não existe</p>
-      <Link href="/"><a>Voltar para home</a></Link>
+
+      <div style={{marginTop: '7rem', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <Image src="/images/404.svg" width="500px" height="500px"/>
+        <p style={{marginBottom: '2.3rem', fontWeight: 'bold', fontSize:'1.2rem'}}>Parece que essa página não existe</p>
+      </div>
     </>
   )
 }
