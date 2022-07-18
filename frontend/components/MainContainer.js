053350a6a7595797107e2ac95e9ee52d6ba2972c
tharsila <tharsilapaula@gmail.com>
2022-07-17 22:29:08 -0300

    feat: next router

--- frontend/components/MainContainer.js
+++ frontend/components/MainContainer.js
@@ -1,12 +1,16 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 import styles from '../styles/MainContainer.module.scss'
+import NavbarAdmin from '../components/NavbarAdmin/NavbarAdmin'
+import {useRouter} from 'next/router'
 
 export default function MainContainer({children}) {
+  const router = useRouter()
+
   return (
     <>
-      <Navbar/>
-        <main id="home" className={styles.container}>{children}</main>
+      {router.pathname === '/' || router.pathname === '/pets' ? <Navbar/> : <NavbarAdmin/> }
+      <main id="home" className={styles.container}>{children}</main>
       <Footer/>
     </>
   )
