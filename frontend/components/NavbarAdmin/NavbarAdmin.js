6cb7d28f136c323881fa25786966f828b78f8dd5
tharsila <tharsilapaula@gmail.com>
2022-07-17 22:29:58 -0300

    feat: Navbar admin

---
+++ frontend/components/NavbarAdmin/NavbarAdmin.js
@@ -0,0 +1,32 @@
+import styles from './NavbarAdmin.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function NavbarAdmin() {
+  return (
+    <>
+      <header className={styles.header}>
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <a style={{display:'flex', alignItems:'center', gap:'1rem'}} >
+              <Image src="/images/logo.PNG" alt={'HelpCat'} width="30px" height="30px" />
+              <span>HelpCat</span>
+            </a>
+          </Link>
+        </div>
+        <div className={styles.menu}>
+          <ul className={styles.menuList}>
+            <li>
+              <Link href="/pets/register"><a>Cadastrar Pet</a></Link>
+            </li>
+            <li>
+              <Link href="/pets/report"><a>Relatorio de Adoções</a></Link> 
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    </>
+  )
+}
