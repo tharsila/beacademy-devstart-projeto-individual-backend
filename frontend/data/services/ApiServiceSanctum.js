2349066fa417d4398d6265e9128760ea895e3597
tharsila <tharsilapaula@gmail.com>
2022-08-01 09:37:57 -0300

    feat: api service

--- frontend/data/services/ApiServiceSanctum.js
+++ frontend/data/services/ApiServiceSanctum.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const ApiService = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+export const ApiServiceSanctum = axios.create({
+  baseURL: 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json'
   },
