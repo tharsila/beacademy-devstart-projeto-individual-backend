db392304aac50054bf4f2d433a94a1bfed1af8a4
tharsila <tharsilapaula@gmail.com>
2022-08-03 22:14:27 -0300

    refactor: apply improvements

--- backend/app/Http/Controllers/PetController.php
+++ backend/app/Http/Controllers/PetController.php
@@ -30,4 +30,63 @@ class PetController extends Controller
 
         return Pet::create($petData);
     }
+
+    /**
+     * Visualiza um pet específico
+     *
+     * @param int $id
+     * @return Pet
+     */
+    public function show($id)
+    {
+        $petId = Pet::find($id);
+        if($petId) {
+            return $petId;
+        }
+
+        return response()->json([
+            'message' => 'Erro ao pesquisar um pet!'
+        ], 404);
+    }
+
+    /**
+     * Atualiza um pet específico
+     *
+     * @param Request $request
+     * @param int $id
+     * @return $pet
+     */
+    public function update(Request $request, $id)
+    {
+        $petDataUpdate = $request->all();
+        $pet = Pet::find($id);
+        if($pet) {
+            $pet->update($petDataUpdate);
+            return $pet;
+        }
+        return response()->json([
+            'message' => 'Erro ao pesquisar um pet!'
+        ], 404);
+    }
+
+    /**
+     * Remove um pet específico
+     *
+     * @param int $id
+     * @return Pet
+     */
+    public function destroy($id)
+    {
+        if(Pet::destroy($id)) {
+            return response()->json([
+                'message' => 'Pet removido com sucesso!'
+            ], 201);
+        }
+
+        return response()->json([
+            'message' => 'Erro ao remover um pet!'
+        ], 404);
+    }
+
+
 }
