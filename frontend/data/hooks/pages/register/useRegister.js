56f66902fd31b400ad3c66727b3185a7b402c103
tharsila <tharsilapaula@gmail.com>
2022-08-03 22:10:40 -0300

    chore: remove useEffect

--- frontend/data/hooks/pages/register/useRegister.js
+++ frontend/data/hooks/pages/register/useRegister.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 import {ApiServiceSanctum} from '../../../services/ApiServiceSanctum'
 
