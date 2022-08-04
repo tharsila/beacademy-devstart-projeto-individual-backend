c67ab6caa569731b28ab05978433f88ed893d6ed
tharsila <tharsilapaula@gmail.com>
2022-08-03 22:05:09 -0300

    chore: change directory

--- frontend/pages/pets/dashboard/index.js
+++
@@ -1,8 +0,0 @@
-import Title from '../../../components/Title/Title'
-export default function Dashboard() {
-  return (
-    <>
-      <Title title={'Pets cadastrados para adoção'} subtitle={'Veja a lista de pets cadastrados'}/>
-    </>
-  )
-}
