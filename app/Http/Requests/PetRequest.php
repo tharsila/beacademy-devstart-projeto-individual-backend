16c1358e86d680bce32e97e966864876381113f6
tharsila <tharsilapaula@gmail.com>
2022-08-04 00:55:47 -0300

    chore: change directory

---
+++ app/Http/Requests/PetRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "name"=> ['required', 'string', 'between:3,100'],
+            'about'=> ['required', 'string'],
+            'image'=> ['required', 'url', 'max:1000'],
+            'location'=>['required', 'string'],
+            'age'=> ['required', 'int'],
+            'sex'=>['required', 'string'],
+            'weigth'=>['required', 'numeric'],
+            'race'=>['required', 'string'],
+            'size'=>['required', 'string'],
+            'since'=>['required', 'string'],
+        ];
+    }
+}
---
+++ app/Rules/UniqueAdoptionPet.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Adoption;
+use Illuminate\Contracts\Validation\Rule;
+
+class UniqueAdoptionPet implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct(private int $petId)
+    {
+
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $alreadyAdopted = Adoption::where('email', $value)
+                ->where('pet_id', $this->petId)
+                ->first();
+        return !$alreadyAdopted;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Você já adotou esse pet!';
+    }
+}
